9d9d89a11f15ac1cc708224c35e044b9da93bdc1
JOHN ORTIZ <johnjairo123451@outlook.com>
2026-08-05 15:54:25 -0500

    Unifica datos y limpia código en clases principales
    Se actualizaron los datos de los objetos en Program.cs para usar el mismo nombre, identificación y fecha de nacimiento, y se ajustaron asignaturas y facultades a áreas de software. Se eliminaron líneas innecesarias y se corrigieron cierres de llaves en DirectorCarrera.cs y Persona.cs. También se quitó una referencia redundante en Estudiantes.cs.

--- Herencias/Program.cs
+++ Herencias/Program.cs
@@ -1,26 +1,26 @@
 ﻿using Herencias.Clases;
 
-Persona objPersona1 = new Persona("0941517790", "John", "Ortiz", new DateOnly(1998, 7, 19));
+Persona objPersona1 = new Persona("0943561177", "Levis", "Mieller", new DateOnly(1998, 7, 18));
 
 Console.WriteLine("Persona 1");
 objPersona1.saludar();
 
 Console.WriteLine();
 Console.WriteLine("Estudiante 1");
-Estudiante objEstudiante1 = new Estudiante("0944567380", "Allan", "Zurtiz", new DateOnly(2000, 2, 15), "Robotica", 1);
+Estudiante objEstudiante1 = new Estudiante("0943561177", "Levis", "Mieller", new DateOnly(1998, 7, 18), "Desarrollo de Software", 2);
 objEstudiante1.saludar();
 objEstudiante1.estudiar();
 
 Console.WriteLine();
 Console.WriteLine("Docente 1");
-Docente objDocente1 = new Docente("0940987652", "Samuel", "Macias", new DateOnly(2001, 8, 7), "Derechos", 6);
+Docente objDocente1 = new Docente("0943561177", "Levis", "Mieller", new DateOnly(1998, 7, 18), "Programación", 5);
 objDocente1.saludar();
 objDocente1.enseñar();
 
 
 Console.WriteLine();
 Console.WriteLine("Director 1");
-DirectorCarrera objDirectorCarrera = new DirectorCarrera("0987651234", "Justin", "Bieber", new DateOnly(1997, 1, 3), "Artes", 5, "Artes musicales");
+DirectorCarrera objDirectorCarrera = new DirectorCarrera("0943561177", "Levis", "Miller", new DateOnly(1998, 7, 18), "Programación", 5, "Ingeniería de Software");
 objDirectorCarrera.saludar();
 objDirectorCarrera.enseñar();
 objDirectorCarrera.dirigir();
